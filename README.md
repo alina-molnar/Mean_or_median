367367b397dc259ce705608b0ed77a07aa38dbb9
Alina Molnar <alina.molnar@outlook.com>
2021-10-04 20:11:41 +0200

    Added Linkedin article and link

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Mean_or_median
 Code for Linkedin article "Mean or median? Real life example."
+
+Read the article at https://www.linkedin.com/pulse/mean-median-real-life-example-alina-molnar/
--- mean_or_median.py
+++ mean_or_median.py
@@ -13,18 +13,19 @@ cash_df = pd.DataFrame(zip(weekdays, amount), columns=["weekday", "amount"])
 
 # Create list of unique amounts to be used as ticks on x axis.
 unique_amount = cash_df["amount"].unique()
+x_lab = range(0, 65, 5)
 
-# Plot amount of cash each day.
-graph = sns.lineplot(x="weekday", y="amount", data=cash_df, marker="o")
-graph.set(title="Amount each day", xlabel="Weekday", ylabel="Amount")
-plt.show()
+# # Plot amount of cash each day.
+# graph = sns.lineplot(x="weekday", y="amount", data=cash_df, marker="o")
+# graph.set(title="Amount each day", xlabel="Weekday", ylabel="Amount")
+# plt.show()
 
 # Plot distribution of amount, mark mean and median.
 graph = sns.kdeplot(x="amount", data=cash_df)
 graph.axvline(x=cash_df["amount"].median(), color="#2ca02c", linestyle="solid", label="median")
 graph.axvline(x=cash_df["amount"].mean(), color="#ff7f0e", linestyle="dashed", label="mean")
 graph.set(title="Distribution mean and median", xlabel="Amount")
-graph.set_xticks(unique_amount)
+graph.set_xticks(x_lab)
 graph.set_xlim([0, 65])
-graph.legend()
+graph.legend(fontsize=25)
 plt.show()
